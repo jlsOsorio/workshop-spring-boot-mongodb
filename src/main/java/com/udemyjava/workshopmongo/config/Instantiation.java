1426c58d56aef0833b673d2235b532dc92994f57
Joao Osorio <joao_lso@hotmail.com>
2022-07-14 19:38:28 +0100

    Posts do utilizador, por referência

--- src/main/java/com/udemyjava/workshopmongo/config/Instantiation.java
+++ src/main/java/com/udemyjava/workshopmongo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("13/07/2022"), "Bom dia", "Acordei cheio de vontade de programar!", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
