d754e702eecfdd03bd77ad17f2bae9552985de53
Joao Osorio <joao_lso@hotmail.com>
2022-07-16 17:07:44 +0100

    Consulta simples com @Query

--- src/main/java/com/udemyjava/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/udemyjava/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.udemyjava.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.udemyjava.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.udemyjava.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //?0 indica que é o primeiro parâmetro do método (0 -> text) que será usado para o regex
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
